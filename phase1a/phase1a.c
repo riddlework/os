8d205741d62392334c63c0a774ef402767031f0c
riddlework <faywagstaff@hotmail.com>
2024-10-03 08:32:07 -0700

    mostly done with phase1a

--- phase1a/phase1a.c
+++ phase1a/phase1a.c
@@ -1,19 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "phase1.h"
-
-
-// how do you create a process?
-// how to tell if a slot in the process table is taken or not?
-// how to keep track of slots in the process table?
-
-// for every function, we must disable interrupts
-    // store psr -- where to store? variable?
-    // disable interrupts
-    // do stuff
-    // restore the state of the running process
-// for every function, must check to confirm that the psr shows you are running
-    // in kernel mode
 
 // parent processes cannot end until all of their children have ended
 // and the parent has collected all of their statues (using join())
@@ -30,11 +18,14 @@ typedef struct node node;
 // process control block
 typedef struct pcb {
     int pid;
+    int status;
     USLOSS_Context context;
+    int (* startFunc) (void *);
+    void * arg;
     struct pcb * parent;
     node * children;
-    int priority; // enforce limit? 1-5 inclusive?
     char process_name[MAXNAME];
+    int priority;
     int is_alive;
 } pcb;
 
@@ -49,19 +40,62 @@ typedef struct node {
 pcb proc_table[MAXPROC];
 
 // current process
-pcb * cur_proc = NULL;
+pcb * cur_proc;
 
-// run queues -- no process will run if a higher priority process is ready to run
-node * run_queue1;
-node * run_queue2;
-node * run_queue3;
-node * run_queue4;
-node * run_queue5;
+// function stubs
+void init_main();
+void Temp_switchTo(int pid);
+int testcase_main_trampoline();
+void quit_phase_1a(int status, int switchToPid);
+
+// returns a pointer to the PCB of the process with the given pid
+pcb * get_proc(int pid) { return &proc_table[pid % MAXPROC]; }
+
+// return the pid of the currently running process
+int get_pid() { return cur_proc->pid; }
+
+
+// find the next empty slot in the proc table
+// assign a pid accordingly
+// return -1 if the proc table is fulle
+int get_next_pid() {
+    int cur_pid = get_pid();
+    int next_pid = -1;
+    for (int i = cur_pid + 1; i % MAXPROC != cur_pid; i++) {
+        pcb proc = proc_table[i & MAXPROC];
+        if (!proc.is_alive) {
+            next_pid = i;
+            break;
+        }
+    } return next_pid;
+}
+
+
+// add to the head of a linked list
+node * add_to_head(pcb * proc, node * list) {
+    node * head;
+    head->process = proc;
+    head->next = list;
+    return head;
+}    
 
 
 // initialize data structures such as the process table
 void phase1_init(void) {
+
     // cannot block or context switch
+
+    // store the current psr
+    unsigned int old_psr = USLOSS_PsrGet();
+
+    // check that we are running in kernel mode -- if not, halt
+    if ((old_psr & USLOSS_PSR_CURRENT_MODE) == 0) {
+        printf("Code in phase1_init is being called from user mode\n");
+        USLOSS_Halt(1);
+    }
+
+    // disable interrupts
+    int psr_status = USLOSS_PsrSet(old_psr & ~USLOSS_PSR_CURRENT_INT); // might cause bugs?
 
     // initialize process table to zeros
     memset(proc_table, 0, sizeof(proc_table));
@@ -69,63 +103,158 @@ void phase1_init(void) {
     // initialize process table entry for init process
     // init always has pid 1
     // never dies
-    pcb init = proc_table[0]; 
-    init.pid = 1;
-    init.priority = 6;
-    init.is_alive = 1;
+    pcb * init = get_proc(1);
+    init->pid = 1;
+    init->priority = 6;
+    init->is_alive = 1;
+    cur_proc = init;
 
-    // call spork once to create the testcase_main process
+    // initialize a context for init
+    // why should this be a char *?
+    char * initStack = malloc(USLOSS_MIN_STACK);
+    USLOSS_ContextInit(&init->context, initStack, USLOSS_MIN_STACK, NULL, init_main);
 
-    // loop over join to clean up process table
-    // keep calling it unless you receive -2 (no children), in which case
-    // print out an error message and terminate the simulation
+    // switch into init
+    Temp_switchTo(1);
 
-    // DO NOT perform context switch 
+    // restore interrupts
+    psr_status = USLOSS_PsrSet(old_psr);
 }
+
+
+// the main function of the init process
+void init_main() {
+    // TODO: disable interrupts and check that we're running in kernel mode
+    
+    // we have now been switched into the init process
+
+    // ask other phases to create processes
+    phase2_start_service_processes();
+    phase3_start_service_processes();
+    phase4_start_service_processes();
+    phase5_start_service_processes();
+    
+    // call spork once to create the testcase_main process
+    int some_pid = spork("testcase_main", testcase_main_trampoline, NULL, USLOSS_MIN_STACK, 3);
+
+
+    // init_main should call spork
+    // spork calls context init and passes the trampoline as a function pointer
+    // the trampoline then retrieves the appropriate function pointer from the pcb
+
+    int join_return;
+    while ((join_return = join(&cur_proc->status)) != -2) {
+        USLOSS_Console("join returned a value of -2 -- terminating simulation");
+        USLOSS_Halt(1);
+    }
+}
+
+
+// a trampoline function for the testcase_main process
+int testcase_main_trampoline() {
+
+    // call testcase_main's main function
+    int ret_val = testcase_main();
+
+    // if it returns, terminate the simulation
+    if (ret_val) USLOSS_Console("nonzero return value... some error detected... halting simulation...");
+    USLOSS_Halt(ret_val);
+
+}
+
+
+// general purpose trampoline function
+void trampoline() {
+
+    // something about enabling interrupts before startFunc is called?
+    
+
+    // pull the function and argument pointers
+    int (*startFunc)(void*) = cur_proc->startFunc;
+    void *arg = cur_proc->arg;
+
+    // call the processes main function
+    int result = startFunc(arg);
+
+    // if startFunc returns, call quit()
+    quit_phase1a(cur_proc->status, 1); // TODO: Change the pid here?
+}
+
 
 // creates a new process, the child of the currently running process
 // startFunc: the main() function for the child process
 // arg: the argument to pass to startFunc
-int spork(char * name, int (* startFunc) (void *), void * arg, int stack_size, int priority) {
-    // may block: no
-    // may context switch: yes
+int spork(char * name, int (*startFunc)(void*), void * arg, int stack_size, int priority) {
+    
+    // store the current psr
+    unsigned int psr_to_restore = USLOSS_PsrGet();
+
+    // check that we are running in kernel mode -- otherwise, halt
+    if ((psr_to_restore & USLOSS_PSR_CURRENT_MODE) == 0) {
+        printf("Code in spork is being called from user mode\n");
+        USLOSS_Halt(1);
+    }
+
+    // disable interrupts
+    int psr_status = USLOSS_PsrSet(psr_to_restore & ~USLOSS_PSR_CURRENT_INT); // might cause bugs?
+
+    // get the child processes' pid, and thus its slot in the proc table
+    int child_pid = get_next_pid();
+    
+    // idx of slot in proc table
+    int slot = child_pid % MAXPROC;
 
     // return -1 if:
     // no empty slots in the process table
     // priority out of range
     // startFunc or name are NULL
     // name too long
-    if (priority < 1 || priority > 5) return -1;
-    if (!startFunc || !name) return -1;
-    if (strlen(name) > MAXNAME) return -1;
-
-    // linear search the process table until you find an empty slot
-    // otherwise there are no empty slots left
+    if (priority < 1 || priority > 5
+            || !startFunc || !name
+            || strlen(name) > MAXNAME
+            || slot <0)
+        return -1;
 
     // return -2 if:
     // stack_size < USLOSS_MIN_STACK
     if (stack_size < USLOSS_MIN_STACK) return -2;
 
 
-    // creates a child process of the current process
-    // creates the entry in the process table and fills it in, then calls the dispatcher
+    // fill slot in proc table
+    pcb * child_proc = get_proc(child_pid);
+    child_proc->pid = child_pid;
+    strcpy(child_proc->process_name,name);
+    child_proc->startFunc = startFunc;
+    child_proc->arg = arg;
+    child_proc->parent = cur_proc; // child of current process
+    child_proc->priority = priority;
+
+    // add new process to cur_proc's children
+    cur_proc->children = add_to_head(child_proc,cur_proc->children);
+
+    // initialize a context for the child process
+    char * stack = malloc(stack_size);
+    USLOSS_ContextInit(&child_proc->context, stack, stack_size, NULL, trampoline);
+
+    // if the child process is a higher priority, run it immediately
+    int cur_pid;
+    if (child_proc->priority < cur_proc->priority) {
+        pcb * old_proc = cur_proc;
+        pcb * new_proc = child_proc;
+
+        cur_proc = new_proc;
+        cur_pid = cur_proc->pid;
+        USLOSS_ContextSwitch(&old_proc->context, &new_proc->context);
+    } 
     
-    // if the child is higher priority than the parent, the child will run before spork() returns
-        // when the child process runs, startFunc() will be called and passed the param specified as arg
-        // if it ever returns, this terminates the process. same effect as calling quit
-        // return value process status
-    
 
-    // STEPS:
-    // find the next empty slot in the proc table using a linear search
-
-
-    // when spork is called, allocate another slot in the table
-        // use it to store information about the process (or return an error)
+    // restore interrupts
+    psr_status = USLOSS_PsrSet(psr_to_restore);
 
     // return PID of child process
-    return cur_proc->pid;
+    return cur_pid;
 }
+
 
 // blocks current process until one of its children has terminated
 // --> then delivers the status of the child (the parameter that the child
@@ -133,6 +262,19 @@ int spork(char * name, int (* startFunc) (void *), void * arg, int stack_size, i
 int join(int * status) {
     // may block: yes
     // may context switch: yes
+    
+    // store the current psr
+    unsigned int psr_to_restore = USLOSS_PsrGet();
+
+    // check that we are running in kernel mode -- otherwise, halt
+    if ((psr_to_restore & USLOSS_PSR_CURRENT_MODE) == 0) {
+        printf("Code in join is being called from user mode\n");
+        USLOSS_Halt(1);
+    }
+    
+    // disable interrupts
+    int psr_status = USLOSS_PsrSet(psr_to_restore & ~USLOSS_PSR_CURRENT_INT); // might cause bugs?
+                                                                          
 
     // fill status with the status of the process joined to
 
@@ -140,27 +282,87 @@ int join(int * status) {
     if (!status) return -3;
     else return 0;
 
+    // restore interrupts
+    psr_status = USLOSS_PsrSet(psr_to_restore);
 }
 
 void quit_phase_1a(int status, int switchToPid) {
+
+    // store the current psr
+    unsigned int psr_to_restore = USLOSS_PsrGet();
+
+    // check that we are running in kernel mode -- otherwise, halt
+    if ((psr_to_restore & USLOSS_PSR_CURRENT_MODE) == 0) {
+        printf("Code in quit_phase_1a is being called from user mode\n");
+        USLOSS_Halt(1);
+    }
+
+    // terminatest the current process with a certain status value
+    // if the parent process is already waiting in a join, the parent process will be awoken
+    // it will continue to take up space in the process table until its status has been delivered
+    // to its parent
+    
+
+    Temp_switchTo(switchToPid);
+
+    // disable interrupts
+    int psr_status = USLOSS_PsrSet(psr_to_restore & ~USLOSS_PSR_CURRENT_INT); // might cause bugs?
+                                                                                   
+    // stuff here
+
+    // restore interrupts
+    psr_status = USLOSS_PsrSet(psr_to_restore);
 }
 
-
-// this function never returns
-// terminates the current process, with a certain "status" value
-// if the parent process is already waiting in a join, then the parent will be
-// awoken.
-// it will continue to take up space in the process table until its status has been
-// delivered to ist parent
-void quit(int status) {
-}
-
-// returns the pid of the currently executing process
-int getpid(void) { return cur_proc->pid; }
 
 // prints human-readable debug data about the process table
 void dumpProcesses(void) {
+
+    // store the current psr
+    unsigned int psr_to_restore = USLOSS_PsrGet();
+
+    // check that we are running in kernel mode -- otherwise, halt
+    if ((psr_to_restore & USLOSS_PSR_CURRENT_MODE) == 0) {
+        printf("Code in dumpPorcesses is being called from user mode\n");
+        USLOSS_Halt(1);
+    }
+
+    // disable interrupts
+    int psr_status = USLOSS_PsrSet(psr_to_restore & ~USLOSS_PSR_CURRENT_INT); // might cause bugs?
+                                                                                   
+    // stuff here
+
+    // restore interrupts
+    psr_status = USLOSS_PsrSet(psr_to_restore);
 }
 
+
 void TEMP_switchTo(int pid) {
+    USLOSS_Console("Phase 1A TEMPORARY HACK: init() manually switching to PID %d.", pid);
+
+    // store the current psr
+    unsigned int psr_to_restore = USLOSS_PsrGet();
+
+    // check that we are running in kernel mode -- otherwise, halt
+    if ((psr_to_restore & USLOSS_PSR_CURRENT_MODE) == 0) {
+        printf("Code in TEMP_switchTo is being called from user mode\n");
+        USLOSS_Halt(1);
+    }
+
+    // retrieve the current process from the prob_table
+    pcb * old_proc = cur_proc; 
+    pcb * new_proc = get_proc(pid);
+    pcb * cur_proc = new_proc;
+
+    // perform context switch
+    USLOSS_ContextSwitch(&old_proc->context,&new_proc->context);
+
+
+    // disable interrupts
+    int psr_status = USLOSS_PsrSet(psr_to_restore & ~USLOSS_PSR_CURRENT_INT); // might cause bugs?
+                                                                                   
+    // stuff here
+
+    // restore interrupts
+    psr_status = USLOSS_PsrSet(psr_to_restore);
 }
