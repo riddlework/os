480c94882157d6ad53072f67bc50a845a9d462b6
riddlework <82472185+riddlework@users.noreply.github.com>
2024-11-06 08:08:20 -0700

    initialized phase3.c

---
+++ phase3/phase3.c
@@ -0,0 +1,40 @@
+//header files
+#include "phase1.h"
+#include "phase2.h"
+#include "usloss.h" // change to <>
+#include "usyscall.h" // change to <>
+
+// may want to include stdlib stuff?
+
+void phase3_init() {
+}
+
+void phase3_start_service_processes() {
+}
+
+void Spawn() {
+    // spork should call a trampoline function that forces the process
+    // into user mode
+}
+
+void Wait() {
+}
+
+void Terminate(){
+}
+
+void SemCreate() {
+}
+
+void SemP() {
+}
+
+void SemV() {
+}
+
+void GetTimeOfDay() {
+}
+
+void GetPID() {
+}
+
