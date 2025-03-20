6d9bc6a3c65823495940525a6a3620f9ed6c50da
riddlework <faywagstaff@hotmail.com>
2025-03-20 08:27:30 -0700

    init phase3

---
+++ phases/phase3/phase3.c
@@ -0,0 +1,32 @@
+#include "phase1.h"
+#include "phase2.h"
+#include "phase3.h"
+
+// function stubs
+
+void phase3_init() {
+}
+
+void phase3_start_service_proesses() {
+}
+
+void SemCreate() {
+}
+
+void SemP() {
+}
+
+void SemV() {
+}
+
+void 
+
+// user mode code must never crash the entire system
+// must perform careful error checking, to ensure params passed to kernel are valid
+
+// disable interrupts only in trampoline for user mode processes
+// disable kernel mode before calling user main function
+
+// semp
+// semv
+
