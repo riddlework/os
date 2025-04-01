5f2156dcf86a3f37100b86f659596a1635c9f4d1
Ben Yurek <benyurek@arizona.edu>
2025-03-31 23:17:33 -0700

    Wrote basic structure of Spawn() trampoline func and mailbox interaction, did basic implementations of Wait() and Terminate(), fixed kernel function name issues, wrote basic structure of P() and V() funcs. More progress needed prior to testing.

--- phases/phase3/phase3.c
+++ phases/phase3/phase3.c
@@ -11,19 +11,20 @@ void phase3_init();
 void phase3_start_service_processes();
 
 // syscall stubs
-void Spawn(USLOSS_Sysargs *arg);
-void Wait(USLOSS_Sysargs *arg);
-void Terminate(USLOSS_Sysargs *arg);
-void SemCreate(USLOSS_Sysargs *arg);
-void SemP(USLOSS_Sysargs *arg);
-void SemV(USLOSS_Sysargs *arg);
-void GetTimeofDay(USLOSS_Sysargs *arg);
-void GetPID(USLOSS_Sysargs *arg);
+void Spawn_K(USLOSS_Sysargs *arg);
+void Wait_K(USLOSS_Sysargs *arg);
+void Terminate_K(USLOSS_Sysargs *arg);
+void SemCreate_K(USLOSS_Sysargs *arg);
+void SemP_K(USLOSS_Sysargs *arg);
+void SemV_K(USLOSS_Sysargs *arg);
+void GetTimeofDay_K(USLOSS_Sysargs *arg);
+void GetPID_K(USLOSS_Sysargs *arg);
 
 // struct definitions
 typedef struct semaphore {
     int semaphore_id;
     int is_alive;
+    int value;
 } Semaphore;
 
 // globals
@@ -61,24 +62,33 @@ void phase3_init() {
     gain_mutex();
 
     // fill system call vector
-    systemCallVec[SYS_SPAWN]        =        Spawn;
-    systemCallVec[SYS_WAIT]         =         Wait;
-    systemCallVec[SYS_TERMINATE]    =    Terminate;
-    systemCallVec[SYS_SEMCREATE]    =    SemCreate;
-    systemCallVec[SYS_SEMP]         =         SemP;
-    systemCallVec[SYS_SEMV]         =         SemV;
-    systemCallVec[SYS_GETTIMEOFDAY] = GetTimeofDay;
-    systemCallVec[SYS_GETPID]       =       GetPID;
+    systemCallVec[SYS_SPAWN]        =        Spawn_K;
+    systemCallVec[SYS_WAIT]         =         Wait_K;
+    systemCallVec[SYS_TERMINATE]    =    Terminate_K;
+    systemCallVec[SYS_SEMCREATE]    =    SemCreate_K;
+    systemCallVec[SYS_SEMP]         =         SemP_K;
+    systemCallVec[SYS_SEMV]         =         SemV_K;
+    systemCallVec[SYS_GETTIMEOFDAY] = GetTimeofDay_K;
+    systemCallVec[SYS_GETPID]       =       GetPID_K;
 
     // release mutual exclusion
     release_mutex();
 }
 
-void phase3_start_service_proesses() {
+void phase3_start_service_processes() {
 }
 
+int trampoline() {
+    // TODO:
+    // Switch to user mode 
+    // Recieve user main function info and args from some mbox
+    // Call user main function and execute the users intended work
+    // Gather and repack return values into arg
+    // Send return values back to user process that executed the syscall
+    // Call terminate to clean up the process
+}
 
-void Spawn(USLOSS_Sysargs *arg) {
+void Spawn_K(USLOSS_Sysargs *arg) {
     // check for kernel mode
     require_kernel_mode(__func__);
 
@@ -92,36 +102,60 @@ void Spawn(USLOSS_Sysargs *arg) {
     int   priority              = (int)(long)       arg->arg4;
     char *name                  = (char *)          arg->arg5;
 
-    int pid = spork(name, start_func, param_to_pass, stack_size, priority);
+    // TODO:
+    // check for valid args (The system cannot be crashed by invalid arguments)
+    // Create a mailbox to send process info 
+    // Send user main proc info and args to the mailbox
 
-    // pack return values
-    arg->arg1 = (void *) &pid;
-
-    // arg4 should be -1 if illegal values were given as input, 0 otherwise
-    // if the process returns the trampoline will automatically call Terminate()
-    // from user mode on behalf of this process, passing as the argument
-    // the value returned from the user-main function.
-
+    // spork the trampoline process
+    int (*tramp_func)(void *)   = (int (*)(void *)) trampoline;
+    int pid = spork(name, tramp_func, param_to_pass, stack_size, priority);
+    
     // release mutex
     release_mutex();
 
 }
 
-void Wait(USLOSS_Sysargs *arg) {
+void Wait_K(USLOSS_Sysargs *arg) {
     // check for kernel mode
     require_kernel_mode(__func__);
 
     // gain mutex
     gain_mutex();
 
+    // unpack arguments
+    int *pid                    = (int *)           arg->arg1;
+    int *status;
+
+    // Call join to get return values
+    int join_pid = join(status);
+
+    arg->arg1 = (void *) join_pid;
+    arg->arg2 = (void *) status;
+
+    // if there are no children, then arg4 = -2. Otherwise 0.
+    arg->arg4 = 0;
+    if (join_pid == -2) {arg->arg4 = -2;}
+
     // release mutex
     release_mutex();
 }
 
-void Terminate(USLOSS_Sysargs *arg) {
+void Terminate_K(USLOSS_Sysargs *arg) {
+    // check for kernel mode
+    require_kernel_mode(__func__);
+
+    // call join until it returns -2 then call quit with the provided status
+    int *quit_status            = (int *)           arg->arg1; 
+    int *status;
+    int join_val = join(status);
+    while (join_val != -2) {
+        join_val = join(status);
+    }
+    quit(quit_status);
 }
 
-void SemCreate(USLOSS_Sysargs *arg) {
+void SemCreate_K(USLOSS_Sysargs *arg) {
     // check for kernel mode
     require_kernel_mode(__func__);
 
@@ -135,11 +169,44 @@ void SemCreate(USLOSS_Sysargs *arg) {
     if (sem_value > MAXSLOTS);
     int mbox_fail = MboxCreate(sem_value, 0);
 
+
     // release mutex
     release_mutex();
 }
 
-void SemP(USLOSS_Sysargs *arg) {
+void SemP_K(USLOSS_Sysargs *arg) {
+    // check for kernel mode
+    require_kernel_mode(__func__);
+
+    // gain mutex
+    gain_mutex();
+
+    // TODO:
+    // Unpack args to get semaphore id
+    // get pointer to the semaphore
+    // decrement the 'value' field
+
+    // release mutex
+    release_mutex();
+}
+
+void SemV_K(USLOSS_Sysargs *arg) {
+    // check for kernel mode
+    require_kernel_mode(__func__);
+
+    // gain mutex
+    gain_mutex();
+
+    // TODO:
+    // Unpack args to get semaphore id
+    // get pointer to the semaphore
+    // increment the 'value' field
+
+    // release mutex
+    release_mutex();
+}
+
+void GetTimeofDay_K(USLOSS_Sysargs *arg) {
     // check for kernel mode
     require_kernel_mode(__func__);
 
@@ -150,29 +217,7 @@ void SemP(USLOSS_Sysargs *arg) {
     release_mutex();
 }
 
-void SemV(USLOSS_Sysargs *arg) {
-    // check for kernel mode
-    require_kernel_mode(__func__);
-
-    // gain mutex
-    gain_mutex();
-
-    // release mutex
-    release_mutex();
-}
-
-void GetTimeofDay(USLOSS_Sysargs *arg) {
-    // check for kernel mode
-    require_kernel_mode(__func__);
-
-    // gain mutex
-    gain_mutex();
-
-    // release mutex
-    release_mutex();
-}
-
-void GetPID(USLOSS_Sysargs *arg) {
+void GetPID_K(USLOSS_Sysargs *arg) {
     // check for kernel mode
     require_kernel_mode(__func__);
 
