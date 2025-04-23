23062097b45b23b3a54b5b13a2acc7c6332ae88a
riddlework <faywagstaff@hotmail.com>
2025-04-23 15:28:41 -0700

    started on termread and write...

--- phases/phase4a/phase4.c
+++ phases/phase4a/phase4.c
@@ -22,10 +22,25 @@
     USLOSS_PsrSet(USLOSS_PsrGet() & ~USLOSS_PSR_CURRENT_MODE); \
 }
 
-// function stubs
+// struct definitions
+typedef struct pcb {
+    int pid;
+    int wakeup_cycle;
+    struct pcb *next;
+} pcb;
+
+typedef struct arg_package {
+    char *buf;
+    int bufSize;
+    int unit;
+} arg_package;
+
+
+/* FUNCTION STUBS */
 void gain_mutex(const char *func);
 void release_mutex(const char *func);
 void phase4_start_service_processes();
+void put_into_sleep_queue(pcb *proc);
 
 // system calls
 void kern_sleep     (USLOSS_Sysargs *arg);
@@ -37,15 +52,7 @@ void kern_disk_size (USLOSS_Sysargs *arg);
 
 // daemons
 int sleepd(void *arg);
-
-// struct definitions
-typedef struct pcb {
-    int pid;
-    int is_alive;
-    int wakeup_cycle;
-    int mbox;
-    struct pcb *next;
-} pcb;
+int termd(void *arg);
 
 // globals
 int mutex;
@@ -73,7 +80,7 @@ void phase4_init() {
     CHECKMODE;
 
     // initialize the mutex using a semaphore
-    int fail = kernSemCreate(1, &mutex);
+    int fail = kernSemCreate(1, &mutex);﻿
     if (fail) USLOSS_Console("ERROR: Failed to create semaphore!\n");
 
     // gain the mutex
@@ -91,7 +98,6 @@ void phase4_init() {
     systemCallVec[SYS_DISKSIZE]  =  kern_disk_size; 
 
 
-
     // TODO:
     // definitely turn on the terminal read (recv) interrupt here
     // maybe turn on the terminal write (xmit) interrupt here -- or only when process is writing?
@@ -105,65 +111,52 @@ void phase4_init() {
 
 void phase4_start_service_processes() {
     // spork the sleep daemon
-    int pid = spork("sleepd", sleepd, NULL, USLOSS_MIN_STACK, 3);
+    spork("sleepd", sleepd, NULL, USLOSS_MIN_STACK, 3);
+    spork("termd", termd, NULL, USLOSS_MIN_STACK, 3);
 }
 
-int sleepd(void *arg) {
-    // call waitDevice here?
-    // waitDevice give syou the current status (time)
-    // every 10 interrupts is one second
+/* DAEMONS */
 
-    gain_mutex(__func__);
+/* sleep daemon */
+int sleepd(void *arg) {
+
     while (1) {
+        // call wait device to wait for a clock interrupt
         int status;
         waitDevice(USLOSS_CLOCK_INT, 0, &status);
         num_cycles_since_start++;
 
+        // wakeup any cycles whose wakeup time has arrived/passed
         while (sleep_head && num_cycles_since_start >= sleep_head->wakeup_cycle) {
+            
+            // gain mutex here since the sleep queue is a shared 
+            gain_mutex(__func__);
             int pid_toUnblock = sleep_head->pid;
             sleep_head = sleep_head->next;
-
             release_mutex(__func__);
 
             unblockProc(pid_toUnblock);
-
-            gain_mutex(__func__);
         }
-
-        release_mutex(__func__);
     }
 }
 
-/* insert a process into the sleep queue */
-void put(pcb *proc) {
+/* terminal daemon */
+int termd(void *arg) {
 
-    // retrieve the cycle number at which the process should wakeup
-    int wakeup_cycle = proc->wakeup_cycle;
+    // waitDevice waits for a terminal interrupt (recv or xmit??)
+    // and reads the status register
+    // we want to write to the control register
+    while (1) {
+        int status;
+        int unit = MboxRecv()
+        waitDevice(USLOSS_TERMI_INT, )
 
-
-    // iterate to the place in the queue at which the process should be inserted
-    pcb *prev = NULL;
-    pcb *cur = sleep_head;
-    while (cur) {
-        int cur_wakeup_cycle = cur->wakeup_cycle;
-        if (wakeup_cycle < cur_wakeup_cycle) break;
-
-        prev = cur;
-        cur = cur->next;
     }
 
-    // insert the process
-    if (prev) {
-        pcb *temp = prev->next;
-        prev->next = proc;
-        proc->next = temp;
-    } else {
-        proc->next = cur;
-        sleep_head = proc;
-    }
 }
 
-// pause the current process for the specified number of seconds
+
+/* pause the current process for the specified number of seconds */
 void kern_sleep(USLOSS_Sysargs *arg) {
     // check for kernel mode
     CHECKMODE;
@@ -181,13 +174,12 @@ void kern_sleep(USLOSS_Sysargs *arg) {
         return;
     }
 
-
     // put the process into the sleep queue before sleeping
     int pid = getpid();
     pcb *cur_proc = &sleep_queue[pid % MAXPROC];
     cur_proc->pid = pid;
     cur_proc->wakeup_cycle = num_cycles_since_start + clock_cycles_to_wait;
-    put(cur_proc);
+    put_into_sleep_queue(cur_proc);
 
     // release mutex before blocking
     release_mutex(__func__);
@@ -198,11 +190,10 @@ void kern_sleep(USLOSS_Sysargs *arg) {
     // regain mutex after blocking
     gain_mutex(__func__);
 
-    // release the mailbox and zero out the slot
-    /*MboxRelease(cur_proc->mbox);*/
+    // zero out the slot so it can be reused
     memset(&sleep_queue[pid % MAXPROC], 1, sizeof(pcb));
 
-    // repack return values
+    // repack success return value
     arg->arg4 = (void *)(long)0;
 
     // release mutex
@@ -221,8 +212,23 @@ void kern_term_read(USLOSS_Sysargs *arg) {
     int   bufSize = (int)(long)arg->arg2;
     int   unit    = (int)(long)arg->arg3;
 
-    // repack return values
+    arg_package args = {
+        .buf     =     buf,
+        .bufSize = bufSize,
+        .unit    =    unit
+    };
+
+    // send the arguments to the term daemon
+    // TODO: Change the mbox number here
+    MboxSend(0, &args, sizeof(arg_package));
+
+
+    // recv the length back from the term daemon
+    // TODO: change the mbox number here
     int lenOut;
+    MboxRecv(0, &lenOut, sizeof(int));
+
+    // repack return values
     arg->arg4 = (void *)(long)lenOut;
 
     // release mutex
@@ -241,8 +247,22 @@ void kern_term_write(USLOSS_Sysargs *arg) {
     int   bufSize = (int)(long)arg->arg2;
     int   unit    = (int)(long)arg->arg3;
 
-    // repack return value
+    arg_package args = {
+        .buf     =     buf,
+        .bufSize = bufSize,
+        .unit    =    unit
+    };
+
+    // send the arguments to the term daemon
+    // TODO: Change the mbox number here
+    MboxSend(0, &args, sizeof(arg_package));
+
+    // recv the length back from the term daemon
+    // TODO: change the mbox number here
     int lenOut;
+    MboxRecv(0, &lenOut, sizeof(int));
+
+    // repack return value
     arg->arg4 = (void *)(long)lenOut;
 
     // release mutex
@@ -317,4 +337,34 @@ void kern_disk_size(USLOSS_Sysargs *arg) {
 
     // release mutex
     release_mutex(__func__);
+}
+
+/* HELPER FUNCTIONS */
+
+/* insert a process into the sleep queue */
+void put_into_sleep_queue(pcb *proc) {
+
+    // retrieve the cycle number at which the process should wakeup
+    int wakeup_cycle = proc->wakeup_cycle;
+
+    // iterate to the place in the queue at which the process should be inserted
+    pcb *prev = NULL;
+    pcb *cur = sleep_head;
+    while (cur) {
+        int cur_wakeup_cycle = cur->wakeup_cycle;
+        if (wakeup_cycle < cur_wakeup_cycle) break;
+
+        prev = cur;
+        cur = cur->next;
+    }
+
+    // insert the process into the sleep queue
+    if (prev) {
+        pcb *temp = prev->next;
+        prev->next = proc;
+        proc->next = temp;
+    } else {
+        proc->next = cur;
+        sleep_head = proc;
+    }
 }
